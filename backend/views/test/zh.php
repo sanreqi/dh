69f6cc14b84dd4e0c9191d8de0389866f66b6b87
shiruiqing <shiruiqing@chinadep.com>
2023-09-08 16:06:34 +0800

    modify

--- backend/views/test/zh.php
+++ backend/views/test/zh.php
@@ -15,4 +15,17 @@ zh ----  ss
         jsApiList: ["startAutoLBS","stopAutoLBS"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
 
+    wx.invoke('startAutoLBS',{
+        type: 'gcj02', // wgs84是gps坐标，gcj02是火星坐标
+        continue:  1, // 默认关闭，值为1的时候启用。页面关闭后，也可继续获取成员的位置信息。需在“应用详情” - “接收消息”页面配置“实时位置信息事件”回调接口，此参数才会生效。
+    }, function(res) {
+        if (res.err_msg == "startAutoLBS:ok") {
+            //调用成功
+            alert('success');
+        } else {
+            alert('fail');
+            //错误处理
+        }
+    });
+
 </script>
