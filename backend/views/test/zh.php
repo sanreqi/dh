9516eb7fa30d16534cd1016042534a4fc63bc4ba
shiruiqing <shiruiqing@chinadep.com>
2023-09-08 17:55:19 +0800

    modify

--- backend/views/test/zh.php
+++ backend/views/test/zh.php
@@ -46,10 +46,11 @@ zh ----  ss
             type: 'wgs84', // wgs84是gps坐标，gcj02是火星坐标
             continue:  1, // 默认关闭，值为1的时候启用。页面关闭后，也可继续获取成员的位置信息。需在“应用详情” - “接收消息”页面配置“实时位置信息事件”回调接口，此参数才会生效。
         }, function(res) {
+            alert(JSON.stringify(res));
             if (res.err_msg == "startAutoLBS:ok") {
                 //调用成功
                 // alert('success');
-                alert(JSON.stringify(res));
+
             } else {
                 alert('fail');
                 //错误处理
