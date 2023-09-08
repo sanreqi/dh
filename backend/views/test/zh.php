54ab51482ead2d48d0c2731899d4e6aeaa094c2a
shiruiqing <shiruiqing@chinadep.com>
2023-09-08 16:19:16 +0800

    modify

--- backend/views/test/zh.php
+++ backend/views/test/zh.php
@@ -15,6 +15,22 @@ zh ----  ss
         jsApiList: ["startAutoLBS","stopAutoLBS"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
     });
 
+    wx.getLocation({
+        type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+        success: function (res) {
+            var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+            alert(latitude);
+            var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+            var speed = res.speed; // 速度，以米/每秒计
+            var accuracy = res.accuracy; // 位置精度
+            var gps_status =  res.gps_status; //gps状态，-1：应用未获取GPS权限；
+                                              // 0：已获取GPS权限，GPS信号异常；
+                                              // 1：已获取GPS权限，GPS信号正常，AGPS信号异常；
+                                              // 2：已获取GPS权限，GPS信号异常，AGPS信号正常；
+                                              // 3：已获取GPS权限，GPS/AGPS信号正常
+        }
+    });
+
     wx.invoke('startAutoLBS',{
         type: 'gcj02', // wgs84是gps坐标，gcj02是火星坐标
         continue:  1, // 默认关闭，值为1的时候启用。页面关闭后，也可继续获取成员的位置信息。需在“应用详情” - “接收消息”页面配置“实时位置信息事件”回调接口，此参数才会生效。
