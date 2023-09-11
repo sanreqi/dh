dbcc60d55dd3aeb51ee7d38b408c17258076c5d6
shiruiqing <shiruiqing@chinadep.com>
2023-09-11 09:58:58 +0800

    modify

--- backend/views/test/zh.php
+++ backend/views/test/zh.php
@@ -9,7 +9,7 @@ zh ----  ss
     wx.config({
         beta: true,// 调用wx.invoke形式的接口值时，该值必须为true。
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        appId: "wwb9164107d1885dd1", // 必填，政务微信的cropID
+        appId: "<?php echo $appId ?>", // 必填，政务微信的cropID
         timestamp: "<?php echo $timestamp ?>", // 必填，生成签名的时间戳
         nonceStr: "<?php echo $nonceStr ?>", // 必填，生成签名的随机串
         signature: "<?php echo $signature ?>",// 必填，签名，见附录1
