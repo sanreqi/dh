a6892114e8a2766b3a4261668cba88ba27668d6d
shiruiqing <shiruiqing@chinadep.com>
2023-09-25 15:11:54 +0800

    modify

--- backend/views/test/zh.php
+++ backend/views/test/zh.php
@@ -4,7 +4,7 @@ zh ----  ss
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
 
 
 <script>
